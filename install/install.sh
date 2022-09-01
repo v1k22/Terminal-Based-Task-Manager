2aa13fa5e177d6aee5395e0c15d40e142a12b0c1
bhavikdhandhalya <dhandhalyabhavik@gmail.com>
2022-09-01 19:02:21 +0530

    executable working with bashrc

---
+++ install/install.sh
@@ -0,0 +1,20 @@
+echo '----------Installing--------------'
+
+mkdir -p /etc/bd
+
+tee -a /root/.bashrc << EOF
+# text inserted by TM - Task Manager program
+display_date(){
+        python3 /root/bd/main.py --summary
+}
+PS1='\${debian_chroot:+(\$debian_chroot)}\u@\h:\w\\$ \n [\$(display_date)] 🦁 '
+# END of text insert by TM
+EOF
+
+echo 'Copying executables to /usr/bin'
+cp ../dist/* /usr/bin/tm
+
+echo '\n\n'
+echo 'Run source ~/.bashrc to apply changes'
+
+source /root/.bashrc
